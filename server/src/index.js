36315d1e5595e71d296a4e6c9f4315a32fe411e5
cyphervoid1232 <tharit_8@hotmail.com>
2018-04-20 17:04:12 +0700

    basic rest server

---
+++ server/src/index.js
@@ -0,0 +1,22 @@
+var express = require('express');
+var app = express();
+var router = express.Router();
+var bodyParser = require('body-parser');
+var bears = [
+    {id:'1', name:"JJ"},
+    {id:'2', name:"Chatarin"},
+    {id:'3', name:"Eiei"}
+];
+router.route('/bears')
+    .get(function(req,res) {
+        res.json(bears);
+    })
+    .post(function(req, res) {
+        var bear = {};
+        bear.name = req.body.name;
+        bears.push(bear);
+        res.json({ message: 'Bear created!' });
+    });
+// all of our routes will be prefixed with /api
+app.use('/api', bodyParser.json(), router);
+app.listen(8000);
