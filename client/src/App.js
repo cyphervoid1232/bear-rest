f2d3cba5e9bfb6a96c0430a2f5ebd2b5dfec8bc0
cyphervoid1232 <tharit_8@hotmail.com>
2018-04-20 17:18:45 +0700

    list bear

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,30 @@
 import React, { Component } from 'react';
+import axios from 'axios'
 
 class App extends Component {
+  constructor(props){
+    super(props)
+    this.state = {data: []};
+  }
+
+  componentDidMount(){
+    axios.get('http://localhost:8000/api/bears')
+    .then( (response) => {
+      this.setState({data:response.data});
+      })
+      .catch( (error) => {
+      console.log(error);
+      });
+  }
+  
   render() {
+    let bears = this.state.data;
     return (
-      <div>Hello World eiei</div>
+      <div>
+      {
+        bears.map(bear => (<div key={bear.id}>{bear.name}</div>))
+      }
+      </div>
     );
   }
 }
--- server/src/index.js
+++ server/src/index.js
@@ -2,6 +2,7 @@ var express = require('express');
 var app = express();
 var router = express.Router();
 var bodyParser = require('body-parser');
+var cors = require('cors');
 var bears = [
     {id:'1', name:"JJ"},
     {id:'2', name:"Chatarin"},
@@ -18,5 +19,7 @@ router.route('/bears')
         res.json({ message: 'Bear created!' });
     });
 // all of our routes will be prefixed with /api
+app.use(cors());
 app.use('/api', bodyParser.json(), router);
+
 app.listen(8000);
